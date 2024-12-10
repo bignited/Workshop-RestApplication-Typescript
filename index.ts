03f9741ecd9b71d00d5247f43e7a10d8f303e49e
Roland <roland.thuys@bignited.be>
2024-12-10 08:58:35 +0100

    add friends calls

--- index.ts
+++ index.ts
@@ -3,6 +3,8 @@ import express, { Request, Response } from 'express';
 const app = express();
 const port = 3000;
 
+const friends: string[] = [];
+
 // Middleware to parse JSON request bodies
 app.use(express.json());
 
@@ -12,9 +14,20 @@ app.get('/hello', (req: Request, res: Response) => {
 });
 
 // POST endpoint
-app.post('/greet', (req: Request, res: Response) => {
+app.post('/hello', (req: Request, res: Response) => {
   const name = req.body.name;
   res.send(`Hello, ${name}!`);
+});
+
+// POST friends endpoint
+app.post('/friend', (req: Request, res: Response) => {
+  friends.push(req.body.friend);
+  res.send(`You have ${friends.length} friends!`);
+});
+
+// GET friends endpoint
+app.get('/friends', (req: Request, res: Response) => {
+  res.send(friends);
 });
 
 app.listen(port, () => {
